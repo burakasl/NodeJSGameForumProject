a448cbeece1ae86cb9da8876185d58e67b8c8516
Burak Aslan <114955804+burakasl@users.noreply.github.com>
2025-04-10 19:08:00 +0300

    Create game insertion schema

--- src/schemas/databaseMapping.js
+++ src/schemas/databaseMapping.js
@@ -2,4 +2,12 @@ const dbMappingSchemas = {};
 
 dbMappingSchemas.user = ["username", "email", "password", "phone"];
 
+dbMappingSchemas.game = [
+    "name",
+    "releaseDate",
+    "description",
+    "score",
+    "imageUrl",
+];
+
 module.exports = dbMappingSchemas;
